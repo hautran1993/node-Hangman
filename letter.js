89f64c8b18f119a04feed8b0b494e69182b388e6
Your Name <hautran1993@yahoo.com>
2017-12-01 18:24:08 -0600

    psuedo coding

---
+++ letter.js
@@ -0,0 +1,15 @@
+var inquirer = require("inquirer")
+
+function Word() {
+  this.rightGuesses = rGuesses;
+  this.wronguesses = wGuesses;
+  this.words = word;
+  this.letters = letters
+}
+
+inquirer.prompt([
+  {
+    message: "would you like to play a game ?"
+  }
+
+]).
---
+++ word.js
@@ -0,0 +1,5 @@
+function Word() {
+//create an array of words
+//by using randomword function
+//check aray for random words
+}
